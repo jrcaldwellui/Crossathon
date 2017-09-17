2daf3787d42d9eeab23cf85e552aac93435d3008
colemtg <cole-pierce@uiowa.edu>
2017-09-17 12:02:45 -0500

    changed file reading

--- app/src/main/java/com/example/nicklitterio/crosswordnew/MainActivity.java
+++ app/src/main/java/com/example/nicklitterio/crosswordnew/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.content.Intent;
 
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
