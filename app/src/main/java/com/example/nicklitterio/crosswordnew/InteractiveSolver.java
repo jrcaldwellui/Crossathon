76fbaa16e1fd19635411c3aa8cf0d0311c33362c
niklitt <nicklitterio@gmail.com>
2017-09-17 12:03:57 -0500

    check read

--- app/src/main/java/com/example/nicklitterio/crosswordnew/InteractiveSolver.java
+++ app/src/main/java/com/example/nicklitterio/crosswordnew/InteractiveSolver.java
@@ -2,6 +2,7 @@ package com.example.nicklitterio.crosswordnew;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import java.io.IOException;
 
 public class InteractiveSolver extends AppCompatActivity {
 
@@ -9,6 +10,11 @@ public class InteractiveSolver extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_interactive_solver);
+        try {
+            Driver.main();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 
